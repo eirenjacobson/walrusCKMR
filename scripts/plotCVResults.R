3ff4ff06eb4e2b540ffbf50cafc7f5d9646c7422
Dave Miller <dave.miller@bioss.ac.uk>
2024-12-19 09:06:26 +0000

    plot now amazing

--- scripts/plotCVResults.R
+++ scripts/plotCVResults.R
@@ -2,6 +2,8 @@ library(tidyr)
 library(dplyr)
 library(ggplot2)
 
+load("../results/design_df.RData")
+
 estdf <- filter(design_df, Value == "Est", CKMR == "Yes") %>%
   select(ID, CKMR, Value, D, L, S, Nfad_2015, Nfad_2020, Nfad_2025) %>%
   pivot_longer(cols = c(Nfad_2015, Nfad_2020, Nfad_2025), names_to = "Year", values_to = "Est") %>%
@@ -11,8 +13,8 @@ cvdf <- na.omit(filter(design_df, Value == "CV")) %>%
   select(ID, CKMR, Value, D, L, S, Nfad_2015, Nfad_2020, Nfad_2025) %>%
   pivot_longer(cols = c(Nfad_2015, Nfad_2020, Nfad_2025), names_to = "Year", values_to = "CV") %>%
   mutate(Year = substr(Year, 6, 9)) %>%
-  mutate(CKMR = factor(CKMR, ordered = TRUE, levels = c("No", "Yes"))) %>%
-  mutate(L = ordered(L, ordered = TRUE, levels = c("No", "Yes")))
+  mutate(CKMR = as.factor(CKMR)) %>%
+  mutate(L = factor(L, levels=2:1, labels=c("Yes", "No")))
 
 sedf <- na.omit(filter(design_df, Value == "SE")) %>%
   select(ID, CKMR, Value, D, L, S, Nfad_2015, Nfad_2020, Nfad_2025) %>%
@@ -31,38 +33,35 @@ ggplot() +
 
 ############### Plot of CVs on N across years, sampling, demo scen, lethal, ckmr
 
-  sampling_names <- list(
-    '1'="+ 100% '25-'26",
-    '2'="+ 100% '25-'27",
-    '3'="+ 100% '25-'28",
-    '4'="+ 75% '25-'26",
-    '5'="+ 75% through '27",
-    '6'="+ 75% through '28",
-    '7'="100% '23-'25"
-  )
-  
-  demo_names <- list(
-    '1' = "Stable Population",
-    '2' = "Decreasing Population",
-    '3' = "Increasing Population"
-  )
-  
-  plot_labeller <- function(variable,value){
-    if (variable=='S') {
-      return(sampling_names[value])
-    } else {
-      return(demo_names[value])
-    }
+sampling_names <- list(
+  '1'="+ 100% '25-'26",
+  '2'="+ 100% '25-'27",
+  '3'="+ 100% '25-'28",
+  '4'="+ 75% '25-'26",
+  '5'="+ 75% through '27",
+  '6'="+ 75% through '28",
+  '7'="100% '23-'25"
+)
+
+demo_names <- list(
+  '1' = "Stable Population",
+  '2' = "Decreasing Population",
+  '3' = "Increasing Population"
+)
+
+plot_labeller <- function(variable,value){
+  if (variable=='S') {
+    return(sampling_names[value])
+  } else {
+    return(demo_names[value])
   }
-  
-  
+}
+
 
 ggplot() +
-  geom_point(data = cvdf, aes(x=Year, y = CV, color = as.factor(L), shape = as.factor(CKMR)))+
+  geom_point(data = cvdf, aes(x=Year, y = CV, color = L, shape = CKMR))+
   facet_grid(D~S, labeller = plot_labeller) +
-  ylab("Expected CV")+
-  scale_color_discrete(name = "Lethal Samples", labels = c("No", "Yes"))+
-  scale_shape_discrete(name = "CKMR", labels = c("Yes", "No")) +
+  labs(colour = "Lethal Samples", shape="CKMR", y="Expected CV") +
   geom_hline(yintercept = 0.15, linetype = "dashed", color = "grey") +
   theme_bw() +
   theme(axis.text.x = element_text(angle = -70,vjust = 0.3, hjust=0)) +
