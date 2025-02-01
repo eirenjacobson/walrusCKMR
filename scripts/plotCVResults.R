e180e47f2e5f4a5c634a17e59a160f9dd0001f70
Eiren Jacobson <eiren.jacobson@gmail.com>
2025-02-01 18:23:51 +0000

    Updated results

--- scripts/plotCVResults.R
+++ scripts/plotCVResults.R
@@ -2,8 +2,9 @@ library(tidyr)
 library(dplyr)
 library(ggplot2)
 library(ggpubr)
+library(PNWColors)
 
-load("./results/design_df.RData")
+load("./results/design_df_filled.RData")
 
 estdf <- filter(design_df, Value == "Est", CKMR == "Yes") %>%
   select(ID, CKMR, Value, D, L, S, Nfad_2015, Nfad_2020, Nfad_2025) %>%
@@ -26,11 +27,30 @@ alldf <- left_join(estdf, sedf, by = c("ID", "CKMR", "D", "L", "S", "Year")) %>%
   mutate(LCI = Est - 1.96*SE) %>%
   mutate(UCI = Est + 1.96*SE)
 
+
 ggplot() +
   geom_point(data = filter(estdf, D == 1), aes(x=Year, y = Est))+
   geom_line(data = filter(estdf, D == 1), aes(x=Year, y = Est))+
   geom_errorbar(data = filter(alldf, D == 1), aes(x=Year, ymin = LCI, ymax = UCI)) +
   facet_grid(~S)
+
+# check mean gain in CV with ICKMR
+
+cvdf %>% 
+  pivot_wider(names_from = CKMR, values_from = CV) %>%
+  mutate(Difference = Yes - No) %>%
+  mutate(Dval = substr(ID, 1, 2)) %>%
+  group_by(Dval) %>%
+  summarize(MeanD = mean(Difference))
+
+# check mean gain in CV with lethal
+
+cvdf %>% 
+  select(-ID) %>%
+  pivot_wider(names_from = L, values_from = CV) %>%
+  mutate(Difference = Yes - No) %>%
+  group_by(interaction(D, CKMR)) %>%
+  summarize(MeanD = mean(Difference))
 
 ############### Plot of CVs on N across years, sampling, demo scen, lethal, ckmr
 
@@ -58,13 +78,15 @@ plot_labeller <- function(variable,value){
   }
 }
 
+pal <- pnw_palette("Starfish", 2)
 
 ggplot() +
   geom_point(data = cvdf, aes(x=Year, y = CV, color = L, shape = CKMR))+
   facet_grid(D~S, labeller = plot_labeller) +
-  labs(colour = "Lethal Samples", shape="CKMR", y="Expected CV") +
-  geom_hline(yintercept = 0.15, linetype = "dashed", color = "grey") +
+  labs(colour = "Lethal Samples", shape="CKMR", y="Expected CV # Adult Females") +
+  geom_hline(yintercept = 0.2, linetype = "dashed", color = "grey") +
   theme_bw() +
+  scale_color_manual(values = c("#105b58", "#ba7999")) +
   theme(axis.text.x = element_text(angle = -70,vjust = 0.3, hjust=0)) +
   theme(legend.position = "bottom")
 
@@ -90,8 +112,10 @@ ggplot(filter(cveffort, Year == 2025)) +
   geom_point(aes(x=Effort, y = CV, color = as.factor(L), shape = as.factor(CKMR))) +
   facet_wrap(~D, nrow = 3, labeller = plot_labeller) +
   labs(colour = "Lethal Samples", shape="CKMR", y="Expected CV") +
-  geom_hline(yintercept = 0.15, linetype = "dashed", color = "grey") +
-  ylab("Expected CV for #Adult Females in 2025")+
+  geom_hline(yintercept = 0.2, linetype = "dashed", color = "grey") +
+  scale_color_manual(values = c("#105b58", "#ba7999")) +
+  
+  ylab("Expected CV for # Adult Females in 2025")+
   xlab("Sampling Effort (Years)")+
   theme_bw() +
   theme(legend.position = "bottom")
@@ -108,9 +132,11 @@ p1 <- cveffort[which(cveffort$Effort==3),] %>%
   pivot_wider(values_from = CV, names_from = S) %>%
   ggplot() +
   geom_abline(intercept = 0, slope = 1, color = "grey") +
-  geom_hline(yintercept = 0.15, linetype = "dashed", color = "grey") +
+  geom_hline(yintercept = 0.2, linetype = "dashed", color = "grey") +
   geom_point(aes(x=`1`, y=`7`,color = as.factor(L), shape = as.factor(CKMR))) +
   labs(colour = "Lethal Samples", shape="CKMR", y="Expected CV") +
+  scale_color_manual(values = c("#105b58", "#ba7999")) +
+  
   facet_wrap(~Year)+
   theme_bw()+
   labs(x = "Expected CV on # Adult Females under Sampling Scenario 1",
@@ -126,8 +152,10 @@ p2 <- cveffort[which(cveffort$Effort==4),] %>%
   pivot_wider(values_from = CV, names_from = S) %>%
   ggplot() +
   geom_abline(intercept = 0, slope = 1, color = "grey") +
-  geom_hline(yintercept = 0.15, linetype = "dashed", color = "grey") +
+  geom_hline(yintercept = 0.2, linetype = "dashed", color = "grey") +
   geom_point(aes(x=`2`, y=`6`, color = as.factor(L), shape = as.factor(CKMR))) +
+  scale_color_manual(values = c("#105b58", "#ba7999")) +
+  
   labs(colour = "Lethal Samples", shape="CKMR", y="Expected CV") +
   facet_wrap(~Year)+
   theme_bw()+
