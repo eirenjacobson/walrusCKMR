fa5f950109d681c2c555df9740e2e3b54727f2f4
Eiren Jacobson <eiren.jacobson@gmail.com>
2025-03-05 12:23:23 +0000

    updated results processing scripts

--- scripts/plotCVResults.R
+++ scripts/plotCVResults.R
@@ -6,6 +6,12 @@ library(PNWColors)
 
 load("./results/design_df_filled.RData")
 
+# take out scenario 7
+design_df <- design_df[-which(design_df$S == "S7"),]
+
+# filter to keep L1 and L3
+design_df <- design_df %>% filter(L == "L1" | L == "L3")
+
 estdf <- filter(design_df, Value == "Est", CKMR == "Yes") %>%
   select(ID, CKMR, Value, D, L, S, Nfad_2015, Nfad_2020, Nfad_2025) %>%
   pivot_longer(cols = c(Nfad_2015, Nfad_2020, Nfad_2025), names_to = "Year", values_to = "Est") %>%
@@ -29,19 +35,20 @@ alldf <- left_join(estdf, sedf, by = c("ID", "CKMR", "D", "L", "S", "Year")) %>%
 
 
 ggplot() +
-  geom_point(data = filter(estdf, D == 1), aes(x=Year, y = Est))+
-  geom_line(data = filter(estdf, D == 1), aes(x=Year, y = Est))+
-  geom_errorbar(data = filter(alldf, D == 1), aes(x=Year, ymin = LCI, ymax = UCI)) +
+  geom_point(data = filter(estdf, D == "D1"), aes(x=Year, y = Est))+
+  geom_line(data = filter(estdf, D == "D1"), aes(x=Year, y = Est))+
+  geom_errorbar(data = filter(alldf, D == "D1"), aes(x=Year, ymin = LCI, ymax = UCI)) +
   facet_grid(~S)
 
-# check mean gain in CV with ICKMR
+# check mean gain in CV with CKMR
 
 cvdf %>% 
   pivot_wider(names_from = CKMR, values_from = CV) %>%
   mutate(Difference = Yes - No) %>%
   mutate(Dval = substr(ID, 1, 2)) %>%
   group_by(Dval) %>%
-  summarize(MeanD = mean(Difference))
+  summarize(MeanD = mean(Difference)) %>%
+  mutate(MeanD = round(MeanD, digits = 2))
 
 # check mean gain in CV with lethal
 
@@ -49,8 +56,9 @@ cvdf %>%
   select(-ID) %>%
   pivot_wider(names_from = L, values_from = CV) %>%
   mutate(Difference = Yes - No) %>%
-  group_by(interaction(D, CKMR)) %>%
-  summarize(MeanD = mean(Difference))
+#  group_by(interaction(D, CKMR)) %>%
+  group_by(CKMR) %>%
+  summarize(MeanD = round(mean(Difference), digits = 2))
 
 ############### Plot of CVs on N across years, sampling, demo scen, lethal, ckmr
 
@@ -59,10 +67,8 @@ sampling_names <- list(
   '2'="+ 100% '25-'27",
   '3'="+ 100% '25-'28",
   '4'="+ 75% '25-'26",
-  '5'="+ 75% through '27",
-  '6'="+ 75% through '28",
-  '7'="100% '23-'25"
-)
+  '5'="+ 75% '25-'27",
+  '6'="+ 75% '25- '28")
 
 demo_names <- list(
   '1' = "Stable Population",
@@ -81,10 +87,10 @@ plot_labeller <- function(variable,value){
 pal <- pnw_palette("Starfish", 2)
 
 ggplot() +
-  geom_point(data = filter(cvdf, L %in% c(0,500)), aes(x=Year, y = CV, color = L, shape = CKMR))+
+  geom_hline(yintercept = 0.2, linetype = "dashed", color = "grey") +
+  geom_point(data = cvdf, aes(x=Year, y = CV, color = L, shape = CKMR))+
   facet_grid(D~S, labeller = plot_labeller) +
   labs(colour = "Lethal Samples", shape="CKMR", y="Expected CV # Adult Females") +
-  geom_hline(yintercept = 0.2, linetype = "dashed", color = "grey") +
   theme_bw() +
   scale_color_manual(values = c("#105b58", "#ba7999")) +
   theme(axis.text.x = element_text(angle = -70,vjust = 0.3, hjust=0)) +
@@ -95,8 +101,8 @@ ggsave(plot = last_plot(), file = "./figures/MegaResults.png",
 
 ##################################### Plot of sampling effort v cv
 
-effort <- data.frame("S" = paste0("S", c(1:7)),
-                     "Effort" = c(3, 4, 5, 2.5, 3.25, 4, 3 ))
+effort <- data.frame("S" = paste0("S", c(1:6)),
+                     "Effort" = c(3, 4, 5, 2.5, 3.25, 4))
 
 cveffort <- left_join(cvdf, effort, by = "S")
 
@@ -108,15 +114,15 @@ plot_labeller <- function(variable,value){
   }
 }
 
-ggplot(filter(cveffort, Year == 2025, L %in% c(0, 500))) + 
+ggplot(filter(cveffort, Year == 2025)) +
+  geom_hline(yintercept = 0.2, linetype = "dashed", color = "grey") +
   geom_point(aes(x=Effort, y = CV, color = as.factor(L), shape = as.factor(CKMR))) +
   facet_wrap(~D, nrow = 3, labeller = plot_labeller) +
   labs(colour = "Lethal Samples", shape="CKMR", y="Expected CV") +
-  geom_hline(yintercept = 0.2, linetype = "dashed", color = "grey") +
   scale_color_manual(values = c("#105b58", "#ba7999")) +
-  
   ylab("Expected CV for # Adult Females in 2025")+
-  xlab("Sampling Effort (Years)")+
+  xlab("Total Survey Effort (Years)")+
+  ylim(c(0, 0.5))+
   theme_bw() +
   theme(legend.position = "bottom")
 
@@ -153,18 +159,19 @@ p2 <- cveffort[which(cveffort$Effort==4),] %>%
   ggplot() +
   geom_abline(intercept = 0, slope = 1, color = "grey") +
   geom_hline(yintercept = 0.2, linetype = "dashed", color = "grey") +
-  geom_point(aes(x=`2`, y=`6`, color = as.factor(L), shape = as.factor(CKMR))) +
+  geom_point(aes(x=`S2`, y=`S6`, color = as.factor(L), shape = as.factor(CKMR))) +
   scale_color_manual(values = c("#105b58", "#ba7999")) +
-  
   labs(colour = "Lethal Samples", shape="CKMR", y="Expected CV") +
   facet_wrap(~Year)+
   theme_bw()+
   labs(x = "Expected CV on # Adult Females under S2",
-       y = "Expected CV on # Adult Females under S6",
-       title = "Four Years of Sampling Effort") +
+       y = "Expected CV on # Adult Females under S6") +
   theme(legend.position = "bottom")
 
-p <- ggarrange(p1, p2, nrow = 2)
+ggsave(plot = p2, file = "./figures/CVw4YrsEff.png", 
+        width = 6, height = 4, units = "in")
 
-ggsave(plot = p, file = "./figures/CVsSameEffort.png", 
-       width = 7, height = 7, units = "in")
+#p <- ggarrange(p1, p2, nrow = 2)
+
+#ggsave(plot = p, file = "./figures/CVsSameEffort.png", 
+#       width = 7, height = 7, units = "in")
--- scripts/plotLHResults.R
+++ scripts/plotLHResults.R
@@ -6,6 +6,12 @@ library(PNWColors)
 
 load("./results/design_df_filled.RData")
 
+# take out scenario 7
+design_df <- design_df[-which(design_df$S == "S7"),]
+
+# filter to keep L1 and L3
+design_df <- design_df %>% filter(L == "L1" | L == "L3")
+
 lhdf <- filter(design_df, Value == "Est") %>%
   select(ID, CKMR, Value, D, L, S, fadsurv, fjusurv, ppn_breedy) %>%
   pivot_longer(cols = c(fadsurv, fjusurv, ppn_breedy), names_to = "Par", 
@@ -19,7 +25,7 @@ cvdf <- na.omit(filter(design_df, Value == "CV")) %>%
   pivot_longer(cols = c(fadsurv, fjusurv, ppn_breedy), names_to = "Par", 
                values_to = "CV") %>%
   mutate(CKMR = as.factor(CKMR)) %>%
-  mutate(L = factor(L, levels=2:1, labels=c("Yes", "No")))
+  mutate(L = factor(L, levels=c("L1", "L3"), labels=c("No", "Yes")))
 
 
 comparecvs <-  na.omit(filter(design_df, Value == "CV")) %>%
@@ -28,10 +34,12 @@ comparecvs <-  na.omit(filter(design_df, Value == "CV")) %>%
                values_to = "CV") %>%
   filter(Par == "ppn_breedy") %>%
   group_by(CKMR) %>%
-#  pivot_wider(names_from = CKMR, values_from = CV) %>%
-#  mutate(Gain = Yes - No) %>%
-  group_by(D) %>%
-  summarize(mean(CV))
+  pivot_wider(names_from = CKMR, values_from = CV) %>%
+  mutate(Gain = Yes - No) %>%summarize(mean(Gain))
+  group_by(D) %>% 
+  summarize(Mean = round(mean(CV), digits = 2), 
+            Min = round(min(CV), digits = 2), 
+            Max = round(max(CV), digits = 2))
 
 ggplot(cvdf) +
   geom_point(aes(x=S, y = Est))+
@@ -41,12 +49,12 @@ ggplot(cvdf) +
 cvdf %>% filter(Par == "fadsurv") %>% 
   group_by(D) %>%
   ggplot() + 
-  geom_point(aes(x=S,y=Est,color = L, shape = CKMR)) + 
+  geom_point(aes(x=S,y=CV,color = L, shape = CKMR)) + 
   facet_wrap(~D)
 
 lhappendix <- na.omit(filter(design_df, Value == "CV")) %>%
   select(D, L, S, CKMR, fadsurv, fjusurv, ppn_breedy) %>%
-  mutate(L = factor(L, levels=2:1, labels=c("Yes", "No"))) %>%
+  mutate(L = factor(L, levels=c("L3", "L1"), labels=c("Yes", "No"))) %>%
   rename("Lethal Samples" = L, "Sampling Scenario" = S,
          "Demographic Scenario" = D) %>%
   mutate(fadsurv = round(fadsurv, digits = 2),
@@ -60,7 +68,7 @@ write.csv(lhappendix, file = "./results/lhappendix.csv")
 
 Nappendix <- na.omit(filter(design_df, Value == "CV")) %>%
   select(D, L, S, CKMR, Nfad_2015, Nfad_2020, Nfad_2025) %>%
-  mutate(L = factor(L, levels=2:1, labels=c("Yes", "No"))) %>%
+  mutate(L = factor(L, levels=c("L3", "L1"), labels=c("Yes", "No"))) %>%
   rename("Lethal Samples" = L, "Sampling Scenario" = S,
          "Demographic Scenario" = D) %>%
   mutate(Nfad_2015 = round(Nfad_2015, digits = 2),
