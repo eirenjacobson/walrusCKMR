7ad1acf1593057b1dfd61b6262102672aa88c65a
Eiren Jacobson <eiren.jacobson@gmail.com>
2025-03-05 08:25:29 +0000

    Manuscript updates

---
+++ docus/lyx2doc.R
@@ -0,0 +1,12 @@
+# wrapper around lyxport for those not afraid of the keyboard
+lick_my_doc <- function(file){
+  # first convert to lyxgz ("lick giz"?)
+  cmd <- paste("lyx --export lyxgz", file)
+  r <- system(cmd)
+  
+  if(r==0){
+    lyxport::lyxzip2word(file)
+  }else{
+    stop("oh no, something bad happened!")
+  }
+}
